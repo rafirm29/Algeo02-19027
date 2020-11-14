be139286068a48a2766ec8f7f6ae5b6e2470c043
haikallf <haikallzrd@gmail.com>
2020-11-15 00:18:56 +0700

    Added Webscrapping

--- src/project/webScrapping.py
+++ src/project/webScrapping.py
@@ -12,15 +12,10 @@ def webScrapping(web):
 
     r = requests.get(web)
 
-    # Initialize BeautifulSoup object to parse the content 
     soup = BeautifulSoup(r.content, 'html.parser')
-
-    # Retrieve all paragraphs and combine it as one
     sen = []
     for i in soup.find('div', {'class':'read__content'}).find_all('p'):
         sen.append(i.text)
-
-    # Add the combined paragraphs to documents
     documents.append(' '.join(sen))
     namafile = str(input("Masukkan nama file yang ingin ditulis (dalam txt): "))
     file1 = open("C:/Users/Haikal/Documents/Kampus/Algeo/Tubes2/Algeo02-19027/src/test/" + namafile + ".txt","w+")
