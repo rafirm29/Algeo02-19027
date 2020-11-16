eb4927f1cfbdca224c847870e39172ac55a498eb
harithfakhiri <68526483+harithfakhiri@users.noreply.github.com>
2020-11-16 11:46:47 +0700

    Update webScrapping.py

--- src/project/webScrapping.py
+++ src/project/webScrapping.py
@@ -3,10 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import string
 
-from textprocessing import inputKata, gabungarray, listToString, removeduplicatex, jumlahKata
-from clean_txt import clean_file, stem
-from cosinesim import sim
-
+#webscrapping
 def webScrapping(web, namafile):
     documents = []
 
